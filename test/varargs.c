3e4c97d674799acb9f6870eb044387a4eb762178
Jacob Myers <jacobm2006@outlook.com>
2026-04-06 11:47:30 -0400

    Added basic varargs testing (uses builtins so no comprehensive testing)

---
+++ test/varargs.c
@@ -0,0 +1,30 @@
+// This is simple for testing and learning and will just be run in the console and the output observed for correctness.
+#include "../intf/assert.h"
+#include "../intf/stdio.h"
+#include "../intf/stdlib.h"
+#include "../intf/stdarg.h"
+
+void printints(int n, ...);
+
+
+int main() {
+  printints(5, 1, 2, 3, 4, 5);
+  return 0;
+}
+
+
+void printints(int n, ...) {
+  va_list args;
+  va_start(args, n);
+  // Buffer for printing to console
+  char buffer[16];
+  for (int i = 0; i < n; ++i) {
+    int next = va_arg(args, int);
+    // Print to the console
+    int n = itoa(next, buffer, 16);
+    buffer[n] = '\0';
+    puts(buffer);
+    putc(',');
+  }
+  putc('\n');
+}
