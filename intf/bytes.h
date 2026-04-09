727c6f3cd460737ea0005570dbb13ac45156f8ac
Jacob Myers <jacobm2006@outlook.com>
2026-04-08 21:08:01 -0400

    Changed type of bytes to unsigned long long (previously TB and PB would have been UB due to signed integer overflow)

--- intf/bytes.h
+++ intf/bytes.h
@@ -1,10 +1,10 @@
 #ifndef __BYTES_H
 #define __BYTES_H
 
-#define KB (1 << 10)
-#define MB (1 << 20)
-#define GB (1 << 30)
-#define TB (1 << 40)
-#define PB (1 << 50)
+#define KB (1ull << 10)
+#define MB (1ull << 20)
+#define GB (1ull << 30)
+#define TB (1ull << 40)
+#define PB (1ull << 50)
 
 #endif
